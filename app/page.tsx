46a54b2c899788c3bd68264d5be45c4b712c7b92
sidra farooq <fsidra167@gmail.com>
2024-11-27 14:06:47 +0500

    Add files via upload

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import { lusitana } from '@/app/ui/fonts';
+
 import styles from '@/app/ui/home.module.css';
 import Image from 'next/image';
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Invoices',
+};
 
 
 export default function Page() {
